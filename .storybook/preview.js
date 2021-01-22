0b5e3300cb9ef0348620546b6773e78f9c8fb149
Vijaykumar R Pai <vijaykumarrpai@gmail.com>
2021-01-22 17:16:42 +0530

    Decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import Center from '../src/components/Center/Center';
+import { ThemeProvider, theme, CSSReset, Box } from '@chakra-ui/core';
+
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <CSSReset />
+    <Box m='4'>{story()}</Box>
+  </ThemeProvider>
+))
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
